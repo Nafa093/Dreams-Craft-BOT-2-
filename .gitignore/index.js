96b337308e4c03721dd3b57ccc66fa7558246cf7
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-03-19 17:13:31 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -807,6 +807,18 @@ client.channels.get('424991281458970645').send({embed: unmutelogs})
         });
     }
 	
+	 	 let argument = message.content.split("/clear").slice(1)
+if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
+var person = message.member.permissions // Permission Clear
+function dot() {
+message.channel.bulkDelete(argument);
+};
+function doNot() {
+message.channel.send(" ").then(m => m.delete(20000));
+};
+person.has("MANAGE_MESSAGES") ? dot() : doNot();
+}); 
  });
+
 
 // Fin du Code
