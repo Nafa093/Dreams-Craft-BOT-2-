90e1918c1a2ebc97b5fc1666a2a0d404757ec99c
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-03-19 16:57:56 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -227,7 +227,8 @@ message.channel.send("Bonjour !")
 if(message.content.toLocaleLowerCase() == ("/sortie")){
 message.reply("La prochaine sortie à DisneyLand Paris organisé par Loulouemerick se déroule le 6 mars ! Vous voulez participer ? Contactez Loulouemerick par MP ! Participants actuels : Loulouemerick.");
 }
-	
+});	
+
 // Command de rôle, OFF
 
 //var lumineuxRole = client.guilds.get(message.guild.id).roles.find("name", "Côté Lumineux"); // Rôle Lumineux
@@ -244,10 +245,10 @@ message.reply("La prochaine sortie à DisneyLand Paris organisé par Loulouemeri
  //message.guild.member(message.author).removeRole(obscurRole);
  //}
 
-});	
 // Commande de clear 
 	 
 client.on('message', message =>{
+	
 let argument = message.content.split("/clear").slice(1)
 if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
 var person = message.member.permissions // Permission Clear
@@ -267,9 +268,10 @@ let args = message.content.split(' ')
 args.shift()
 message.channel.send(args.join(' '))
     }
+	
 });	
 
-client.on('message', message =>{
+client.on('message', message => {
 // Tri des insultes automatique 
 	
 const insultelogs = {
