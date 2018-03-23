af4db110ac3d655a326d7da56779fe767b257954
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-03-23 16:19:35 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -819,15 +819,21 @@ client.channels.get('424991281458970645').send({embed: unmutelogs})
     }
  });
 
-	 client.on("message", message => { 
-
- let argument = message.content.split("/clear").slice(1)
- 
+client.on("message", message => { 
+	
+var person = message.member.permissions
+let argument = message.content.split("/clear").slice(1)
 if(!argument) return message.channel.send("Merci d'indiquer de respecter le modèle suivant : ``/clear ( nombre de 0 à 100)``").then(m => m.delete(20000))
- if (!message.member.hasPermission('MANAGE_ROLES'))
-return message.channel.send("Tu n'as pas la permissions !");
+ 
+function dot() {
 message.channel.bulkDelete(argument);
 };
+	
+function doNot() {
+message.channel.send(" ").then(m => m.delete(20000));
+};
+person.has("MANAGE_MESSAGES") ? dot() : doNot();
+
  });
 
 
