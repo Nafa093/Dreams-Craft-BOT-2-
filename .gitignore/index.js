1ae7fbf9a7ea4f23f81828f6a252fdff4379518d
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-07-23 23:48:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,1 @@
-const discord = require('discord.js'),
-client = new discord.Client();
-const token = (process.env.TOKEN);
-var prefix = ".";
-
-
-client.login(token)
-
-client.on('guildMemberAdd', member => {
-const channel = member.guild.channels.find("name", "bienvenue").send(`Bienvenue chez CoasterFan ${member.user} !`)
-});
 
