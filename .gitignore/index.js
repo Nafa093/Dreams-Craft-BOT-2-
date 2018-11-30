8cd88f011f0f86636c02ab4a2d8cb91336e1bcfb
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-12-01 00:05:08 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1121,7 +1121,7 @@ client.on('message', message =>{
 
     if (message.channel.type == 'dm') return;
     if (message.channel.id !== '518190701050069012') return;
-    if (today.getDate() === 1 && today.getMonth() === 11) {
+    if (today.getDateUTC() === 1 && today.getMonthUTC() === 11) {
     message.delete()
     message.channel.send(`Tu viens d'ouvir la case n°1 de ton calendrier ! ${message.author}`)
     message.author.send("Voici le contenue de la première case de votre calendrier : Mon premier est ``8``. Vous remportez également 5 DcCoins !")
