afc05d5f8fc1ad05175bf6233ae1bfc8e7c6f432
Nafada093 <35405928+Nafada093@users.noreply.github.com>
2018-12-12 00:14:38 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1131,17 +1131,17 @@ client.on('message', message =>{
     }
   } 
   
-    if (message.content.toLocaleLowerCase() === `/case 11`) {		 
+    if (message.content.toLocaleLowerCase() === `/case 12`) {		 
  if (message.channel.type == 'dm') return;
  if (message.channel.id !== '518190701050069012') return;
     if (today.getDate() === 11 && today.getMonth() === 11) {
     message.delete()
-    message.channel.send(`Tu viens d'ouvir la case n°11 de ton calendrier ! ${message.author}`)
-    message.author.send("Voici le contenue de la onzième case de votre calendrier : Mon onzième est ``D``. Vous remportez également 2 DcCoins !")
-    client.channels.get('518188746219192320').send(`${message.author} à ouvert la case n°11 !`)
+    message.channel.send(`Tu viens d'ouvir la case n°12 de ton calendrier ! ${message.author}`)
+    message.author.send("Voici le contenue de la douzième case de votre calendrier : Mon douzième est ``D``. Vous remportez également un yaourt à la grec !")
+    client.channels.get('518188746219192320').send(`${message.author} à ouvert la case n°12 !`)
 
     }
-  } 
+  }  
   
   
     if (message.content.toLocaleLowerCase() === `/case 12`) {		 
